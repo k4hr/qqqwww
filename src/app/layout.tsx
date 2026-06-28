cae8800e4a56309911ef04908bd75aa3d069cf07
k4hr <alecsandr-7a@yandex.ru>
2026-06-28 21:59:40 +0300

    2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,23 +53,19 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(m,e,t,r,i,k,a){
-                if (m.__redfilmYandexMetrika110229115) return;
-                m.__redfilmYandexMetrika110229115 = true;
-
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-
-                for (var j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) { return; }
-                }
-
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
+              window.ym = window.ym || function(){
+                (window.ym.a = window.ym.a || []).push(arguments);
+              };
+              window.ym.l = 1 * new Date();
+            `,
+          }}
+        />
+        <script id="yandex-metrika-tag" async src="https://mc.yandex.ru/metrika/tag.js" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               if (!window.location.pathname.startsWith("/admin")) {
-                ym(110229115, "init", {
+                window.ym(110229115, "init", {
                   ssr: true,
                   webvisor: true,
                   clickmap: true,
