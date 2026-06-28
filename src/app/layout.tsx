5b1e3415ca760c552e1629ea73478e4ce0badfa5
k4hr <alecsandr-7a@yandex.ru>
2026-06-28 21:49:38 +0300

    3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,30 +50,37 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <TelegramWebAppBridge />
         <Script id="telegram-webapp-sdk" src="https://telegram.org/js/telegram-web-app.js" strategy="afterInteractive" />
         <Script id="redfilm-vibix-player-sdk" src="https://graphicslab.io/sdk/v2/rendex-sdk.min.js" strategy="afterInteractive" />
-        <Script id="yandex-metrika-counter" strategy="afterInteractive">
-          {`
-            if (!window.location.pathname.startsWith("/admin")) {
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               (function(m,e,t,r,i,k,a){
+                if (m.__redfilmYandexMetrika110229115) return;
+                m.__redfilmYandexMetrika110229115 = true;
+
                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
+
                 for (var j = 0; j < document.scripts.length; j++) {
                   if (document.scripts[j].src === r) { return; }
                 }
+
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],
                 k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
               })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(110229115, "init", {
-                ssr: true,
-                webvisor: true,
-                clickmap: true,
-                ecommerce: "dataLayer",
-                accurateTrackBounce: true,
-                trackLinks: true
-              });
-            }
-          `}
-        </Script>
+              if (!window.location.pathname.startsWith("/admin")) {
+                ym(110229115, "init", {
+                  ssr: true,
+                  webvisor: true,
+                  clickmap: true,
+                  ecommerce: "dataLayer",
+                  accurateTrackBounce: true,
+                  trackLinks: true
+                });
+              }
+            `,
+          }}
+        />
         <noscript>
           <div>
             <img src="https://mc.yandex.ru/watch/110229115" style={{ position: "absolute", left: "-9999px" }} alt="" />
