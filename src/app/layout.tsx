1b060fcb87e2bdf9928a9eaf7bf35b8e516f30a4
k4hr <alecsandr-7a@yandex.ru>
2026-06-28 21:42:39 +0300

    3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,35 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <TelegramWebAppBridge />
         <Script id="telegram-webapp-sdk" src="https://telegram.org/js/telegram-web-app.js" strategy="afterInteractive" />
         <Script id="redfilm-vibix-player-sdk" src="https://graphicslab.io/sdk/v2/rendex-sdk.min.js" strategy="afterInteractive" />
+        <Script id="yandex-metrika-counter" strategy="afterInteractive">
+          {`
+            if (!window.location.pathname.startsWith("/admin")) {
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(110229115, "init", {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                accurateTrackBounce: true,
+                trackLinks: true
+              });
+            }
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/110229115" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <YandexMetrika />
         <Header />
         <main className="cinematic-page">{children}</main>
--- src/components/yandex-metrika.tsx
+++ src/components/yandex-metrika.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Script from "next/script";
 import { usePathname, useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useMemo, useRef } from "react";
 
@@ -10,8 +9,7 @@ declare global {
   }
 }
 
-const RAW_COUNTER_ID = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID || "110229115";
-const COUNTER_ID = Number(RAW_COUNTER_ID);
+const COUNTER_ID = 110229115;
 
 function YandexMetrikaInner() {
   const pathname = usePathname();
@@ -20,8 +18,6 @@ function YandexMetrikaInner() {
 
   const enabled =
     process.env.NODE_ENV === "production" &&
-    Number.isFinite(COUNTER_ID) &&
-    COUNTER_ID > 0 &&
     !pathname?.startsWith("/admin");
 
   const currentUrl = useMemo(() => {
@@ -46,43 +42,7 @@ function YandexMetrikaInner() {
     }
   }, [currentUrl, enabled]);
 
-  if (!enabled) return null;
-
-  return (
-    <>
-      <Script id="yandex-metrika" strategy="afterInteractive">
-        {`
-          (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-              if (document.scripts[j].src === r) { return; }
-            }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-          ym(${COUNTER_ID}, "init", {
-            ssr: true,
-            webvisor: true,
-            clickmap: true,
-            ecommerce: "dataLayer",
-            accurateTrackBounce: true,
-            trackLinks: true
-          });
-        `}
-      </Script>
-      <noscript>
-        <div>
-          <img
-            src={`https://mc.yandex.ru/watch/${COUNTER_ID}`}
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
-    </>
-  );
+  return null;
 }
 
 export function YandexMetrika() {
