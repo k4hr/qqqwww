23f36fb337765414df889d0966262dde5fcc61e4
k4hr <alecsandr-7a@yandex.ru>
2026-07-08 11:30:04 +0300

    Force dynamic MSX player page

--- src/app/msx/player/[slug]/page.tsx
+++ src/app/msx/player/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { TvFocusProvider } from "@/components/tv/tv-focus-provider";
 import { TvCss, TvShell } from "@/components/tv/tv-ui";
 import { getTvMovieBySlug, tvMoviePath } from "@/lib/tv";
 
-export const revalidate = 300;
+export const dynamic = "force-dynamic";
 
 type Props = { params: Promise<{ slug: string }> };
 
