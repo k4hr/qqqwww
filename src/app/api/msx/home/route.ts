d8baa0272749b32784ff716d1343d856a82709e0
k4hr <alecsandr-7a@yandex.ru>
2026-07-08 11:20:54 +0300

    Force dynamic MSX home API

--- src/app/api/msx/home/route.ts
+++ src/app/api/msx/home/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getTvHome, serializeTvMovie } from "@/lib/tv";
 
-export const revalidate = 300;
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const home = await getTvHome();
