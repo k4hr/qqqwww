3c62ffe171865814b868e50a14e99c8fb6bf7f07
k4hr <alecsandr-7a@yandex.ru>
2026-07-14 13:48:05 +0300

    22

--- src/components/player-block.tsx
+++ src/components/player-block.tsx
@@ -1,7 +1,6 @@
 import type { Movie } from "@prisma/client";
 import { VibixPlayer } from "@/components/vibix-player";
 import { AnalyticsEvent } from "@/components/analytics-event";
-import { VibixTrailer } from "@/components/vibix-trailer";
 
 type Props = {
   movie: Pick<Movie, "id" | "titleRu" | "year" | "kinopoiskId" | "imdbId" | "vibixId" | "vibixType" | "vibixIframeUrl" | "vibixEmbedCode" | "posterUrl">;
@@ -33,14 +32,6 @@ export function PlayerBlock({ movie }: Props) {
           posterUrl={movie.posterUrl}
         />
       </div>
-      <VibixTrailer
-        title={movie.titleRu}
-        kinopoiskId={movie.kinopoiskId}
-        imdbId={movie.imdbId}
-        vibixId={movie.vibixId}
-        vibixType={movie.vibixType}
-        embedCode={movie.vibixEmbedCode}
-      />
     </section>
   );
 }
